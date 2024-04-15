3a66bab5ecc7f022336df2d5f75a7f54929e65ac
Ana Atuesta <gabrielaatuesta@gmail.com>
2024-04-15 11:42:41 -0500

    go router configurado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 
 class HomeScreen extends StatelessWidget {
@@ -59,7 +59,8 @@ class _CustomListTile extends StatelessWidget {
         //   ),
         // );
         
-        Navigator.pushNamed(context, menuItem.link);
+        // Navigator.pushNamed(context, menuItem.link);
+        context.push( menuItem.link );
         
       },
     );
---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,5 @@
+// ESTE ARCHIVO SE ENCARGA DE EXPORTAR TODOS MIS SCREENS
+
+export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
+export 'package:widgets_app/presentation/screens/home/home_screen.dart';
